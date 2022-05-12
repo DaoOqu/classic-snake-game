7c7fec7b03dbfb5a9f81712d8bd46de638e10775
David Oquendo <davidoqu23@gmail.com>
2022-05-12 17:48:30 -0500

    Started working on game mechanics

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+// declare global variables to track game board size
+const linePixelCount = 40;
+const totalPixelCount = linePixelCount ** 2;
+
+// track scores and display it
+let totalFoodEaten = 0;
+let totalDistanceTraveled = 0;
+
+const gameContainer = document.querySelector('#game-container');
+
+// generate game board
+const createGameBoardPixels = () => {
+  for(let i = 1; i < totalPixelCount; i++) {
+    gameContainer.innerHTML = `${gameContainer.innerHTML} <div class='gameBoardPixel' id='pixel${i}'></div>`;
+  }
+}
+
+
+
+
+
 
