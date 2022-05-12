1d8820688e20b3110aa6fdf2cb539b64438aad56
David Oquendo <davidoqu23@gmail.com>
2022-05-12 17:51:36 -0500

    Added random food generator, snake behavior, length, starting point, and user input

--- main.js
+++ main.js
@@ -16,6 +16,51 @@ const createGameBoardPixels = () => {
 }
 
 
+// shorten reference to game pixels
+const gameBoardPixels = document.querySelector('.gameBoardPixel');
+
+let currentFoodPosition = 0;
+
+// create the randomly generated food items in the game board
+const createFood = () => {
+  gameBoardPixels[currentFoodPosition].classlist.remove('food');
+  currentFoodPosition = Math.floor(Math.random() * totalPixelCount);
+  gameBoardPixels[currentFoodPosition].classlist.add('food');
+}
+
+// start setting up snake behavior
+const leftDir = 37;
+const upDir = 38;
+const rightDir = 39;
+const downDir = 40;
+
+let snakeCurrentDirection = rightDir;
+
+// make sure that user input is valid and change snake direction variable
+const changeDirection = newDirectionCode => {
+  if(newDirectionCode === snakeCurrentDirection) return;
+
+  if(newDirectionCode === leftDir && snakeCurrentDirection !== rightDir) {
+    snakeCurrentDirection = newDirectionCode;
+  }else if(newDirectionCode === upDir && snakeCurrentDirection !== downDir) {
+    snakeCurrentDirection = newDirectionCode;
+  }else if(newDirectionCode === rightDir && snakeCurrentDirection !== leftDir) {
+    snakeCurrentDirection = newDirectionCode;
+  }else if(newDirectionCode === downDir && snakeCurrentDirection !== upDir) {
+    snakeCurrentDirection = newDirectionCode;
+  }
+}
+
+// set starting point for snake on load
+let currentHeadPosition = totalPixelCount / 2;
+
+// set initial length
+let snakeLength = 200;
+
+// start moving snake, wrap around to other side of screen if needed
+
+
+
 
 
 
